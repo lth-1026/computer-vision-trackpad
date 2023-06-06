4f3eabf78418b90fbc9a1517a006c4e85552ff51
lth-1026 <th20001026@gmail.com>
2023-06-06 20:59:06 +0900

    손가락을 따라 마우스 포인터가 움직이도록 기능 추가

--- cv_trackpad.py
+++ cv_trackpad.py
@@ -1,5 +1,9 @@
+import time
 from cvzone.HandTrackingModule import HandDetector
+from pynput.mouse import Controller
 import cv2
+
+mouse = Controller()
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8, maxHands=1)
@@ -7,6 +11,7 @@ while True:
     # Get image frame
     success, img = cap.read()
     # Find the hand and its landmarks
+    img = cv2.flip(img, 1)
     hands, img = detector.findHands(img)  # with draw
     # hands = detector.findHands(img, draw=False)  # without draw
 
@@ -20,19 +25,12 @@ while True:
 
         fingers1 = detector.fingersUp(hand1)
 
-        if len(hands) == 2:
-            # Hand 2
-            hand2 = hands[1]
-            lmList2 = hand2["lmList"]  # List of 21 Landmark points
-            bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
-            centerPoint2 = hand2['center']  # center of the hand cx,cy
-            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
+        if fingers1[1] == 1 and fingers1[2] != 1 and fingers1[3] != 1 and fingers1[4] != 1 or fingers1[1] == 0 and fingers1[2] != 0 and fingers1[3] != 0 and fingers1[4] != 0:
+            mouse.position = (lmList1[8][0], lmList1[8][1])
 
-            fingers2 = detector.fingersUp(hand2)
+            cv2.putText(img, text=str(lmList1[8]), org=(10, 160), 
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(255,255,255), thickness=2)
 
-            # Find Distance between two Landmarks. Could be same hand or different hands
-            length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)  # with draw
-            # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with draw
     # Display
     cv2.imshow("Image", img)
 
